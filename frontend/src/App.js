295247ad18515eff0524dfed9a0c73eddf5eae65
Murtaza Hassan <murtazahassan_2024@depauw.edu>
2023-11-07 19:21:06 -0500

    working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-// Your existing components
+
 import MainPage from '/Users/murtazahassan/Desktop/StudySync/frontend/src/components/mainpage/MainPage.js'; 
 import Login from '/Users/murtazahassan/Desktop/StudySync/frontend/src/components/login/Login.js';
 import Register from '/Users/murtazahassan/Desktop/StudySync/frontend/src/components/register/Register';
@@ -22,3 +22,4 @@ function App() {
 }
 
 export default App;
+
